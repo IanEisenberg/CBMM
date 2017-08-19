91109f8e55df3add187a8569d5ca72b5452c9c97
ian <ianeisenberg90@gmail.com>
2017-08-18 22:12:03 -0700

    changes

--- data_augmentation.py
+++ data_augmentation.py
@@ -6,13 +6,17 @@ from keras.models import Model
 from keras.optimizers import Adam
 
 from itertools import product
-from matplotlib import pyplot as plt
 import numpy as np
 from os import path
 import pandas as pd
-import seaborn as sns
+import pickle
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import scale
+
+from matplotlib import pyplot as plt
+import matplotlib
+matplotlib.use('GTK')
+import seaborn as sns
 
 data_loc = path.join('Data', 'meaningful_variables_imputed.csv')
 
@@ -76,10 +80,11 @@ def make_autoencoder(input_vec, encoding_dim=100, regularize=None,
     decoder = Model(encoded_input, decoder_layer(encoded_input))
     return autoencoder, encoder, decoder
 
-def run_autoencoder(train, val, dim, l1, epochs=200):
+def run_autoencoder(train, val, dim, l1, dropout, epochs=100):
     input_vec = Input(shape=(data.shape[1],))
     autoencoder, encoder, decoder = make_autoencoder(input_vec, dim, 
-                                                     regularize=l1)
+                                                     regularize=l1,
+                                                     dropout=dropout)
     autoencoder.compile(optimizer=Adam(lr=.1, decay=.001), loss='mse')
     if val is not None:
         val=(val,val)
@@ -89,7 +94,7 @@ def run_autoencoder(train, val, dim, l1, epochs=200):
     # train autoencoder
     out = autoencoder.fit(train, train,
                     epochs=epochs,
-                    batch_size=6000,
+                    batch_size=epochs,
                     shuffle=True,
                     validation_data=val,
                     verbose=0,
@@ -103,29 +108,39 @@ def KF_CV(data, param_space, splits=5):
     KF = KFold(splits)
     folds = list(KF.split(data))
     param_combinations = list(product(param_space['dim'],
-                                      param_space['Wl1']))
+                                      param_space['Wl1'],
+                                      param_space['dropout']))
     param_scores = {}
     # grid search over params
-    for d,l in param_combinations:
-        print('Testing parameters: %s, %s' % (d,l))
-        KV_scores = []
+    for dim,l,dropout in param_combinations:
+        print('Testing parameters: %s, %s, %s' % (dim,l,dropout))
+        CV_scores = []
         for train_i, val_i in folds:
             x_train = scale(data[train_i,:])
             x_val = scale(data[val_i,:])
-            out, models = run_autoencoder(x_train, x_val, d, l)
-            KV_scores.append(out.history['val_loss'][-1])
-        param_scores[(d,l)] = (np.mean(KV_scores))
+            out, models = run_autoencoder(x_train, x_val, dim, l,
+                                          dropout, epochs=10000)
+            final_score = out.history['val_loss'][-1]
+            CV_scores.append(final_score)
+            print('CV score: %s' % final_score)
+        param_scores[(dim,l,dropout)] = (np.mean(CV_scores))
     best_params = min(param_scores, key=lambda k: param_scores[k])
     return best_params, param_scores
     
 # ***************************************************************************
 # autoencoder
 # ***************************************************************************
-param_space = {'dim': [100, 200, 300], 'Wl1': [None, .001, .0001]}
+param_space = {'dim': [50, 150, 250, 350], 'Wl1': [None, .01, .001, .0001],
+               'dropout': [True, False]}
 best_params, param_scores = KF_CV(data_train, param_space, splits=4)
+pickle.dump(param_scores, open(path.join('output', 
+                                         'data_augmentation_CV_results.pkl'), 
+                                         'wb'))
 out, models = run_autoencoder(scale(data_train), None, best_params[0], 
                               best_params[1], epochs=1000)
-
+for name,m in models.items():
+    m.save(path.join('output', 'data_augmentation_%s.h5' % name))
+    
 # ***************************************************************************
 # test
 # ***************************************************************************
@@ -137,22 +152,27 @@ encoded_imgs = models['encoder'].predict(test_data)
 decoded_imgs = models['decoder'].predict(encoded_imgs)
 corr = pd.DataFrame(np.vstack([test_data,decoded_imgs])).T.corr().values
 np.mean(np.diag(corr[n_test:,:n_test]))
-# plot
-plt.figure(figsize=(12,8))
-sns.heatmap(corr)
+
 
 # augment data
 augmented_data = autoencoder_augmentation(models['encoder'], 
                                           models['decoder'], 
                                           scale(data))
 
+# save augmented data
+augmented_data = pd.DataFrame(augmented_data, columns=data_df.columns)
+augmented_data.to_csv(path.join('output','augmented_data.csv'))
+
+# plot
+f = plt.figure(figsize=(12,8))
+sns.heatmap(corr)
+f.savefig(path.join('Plots','test_reconsturction_performance.png'))
+
 # compare augmented data RSA to original data space
-plt.figure(figsize=(12,8))
+f=plt.figure(figsize=(12,8))
 plt.scatter(tril(np.corrcoef(scale(data).T)), 
             tril(np.corrcoef(augmented_data.T)))
 plt.xlabel('Original Data Correlations', fontsize=20)
 plt.ylabel('Augmented Data Correlations', fontsize=20)
+f.savefig(path.join('Plots','augmented_data_corr_comparison.png'))
 
-# save augmented data
-augmented_data = pd.DataFrame(augmented_data, columns=data_df.columns)
-augmented_data.to_csv(path.join('Data','augmented_data.csv'))
